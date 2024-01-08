795b4a9e4f27bd24c2d103196fc700c0e4ef6bfb
kevinmulier <kevin.mulier@outlook.fr>
2024-01-08 16:15:50 +0100

    complete exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [persons, setPersons] = useState([
+		{ name: "Arto Hellas", number: "040-1234567" },
+	]);
 	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
 
 	const addContact = (event) => {
 		event.preventDefault();
@@ -16,17 +19,24 @@ const App = () => {
 
 		const personObject = {
 			name: newName,
+			number: newNumber,
 		};
+
 		setPersons(persons.concat(personObject));
 		setNewName("");
+		setNewNumber("");
 	};
 
 	const handleNameChange = (event) => {
 		setNewName(event.target.value);
 	};
 
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value);
+	};
+
 	return (
-		<div>
+		<>
 			<h2>Phonebook</h2>
 			<form onSubmit={addContact}>
 				<div>
@@ -37,15 +47,26 @@ const App = () => {
 					/>
 				</div>
 				<div>
+					number:{" "}
+					<input
+						onChange={handleNumberChange}
+						value={newNumber}
+					/>
+				</div>
+				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
 			{persons.map((person) => (
-				<p key={person.name}>{person.name}</p>
+				<p key={person.name}>
+					{person.name} {person.number}
+				</p>
 			))}
-			<div>debug: {newName}</div>
-		</div>
+			<div>
+				debug: {newName} {newNumber}
+			</div>
+		</>
 	);
 };
 
