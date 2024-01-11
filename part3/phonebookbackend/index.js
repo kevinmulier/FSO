99cb929ab77a8db4445eba9ea78a126b0451184c
kevinmulier <kevin.mulier@outlook.fr>
2024-01-11 22:01:46 +0100

    complete exercise 3.8

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -3,7 +3,16 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("content", function getContent(req) {
+	return JSON.stringify(req.body);
+});
+
+app.use(
+	morgan(
+		":method :url :status :res[content-length] - :response-time ms :content",
+	),
+);
 
 let persons = [
 	{
