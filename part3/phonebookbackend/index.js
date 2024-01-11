1416ca87f10a0dea7c7bc640b6e94cac7d5089e0
kevinmulier <kevin.mulier@outlook.fr>
2024-01-11 18:09:48 +0100

    complete exercise 3.5

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
 	{
 		id: 1,
@@ -38,6 +40,15 @@ app.get("/api/persons/:id", (request, response) => {
 	} else {
 		response.status(404).end();
 	}
+});
+
+app.post("/api/persons", (request, response) => {
+	const newPerson = request.body;
+	newPerson.id = Math.floor(Math.random() * 1000000);
+
+	persons = persons.concat(newPerson);
+
+	response.json(newPerson);
 });
 
 app.delete("/api/persons/:id", (request, response) => {
