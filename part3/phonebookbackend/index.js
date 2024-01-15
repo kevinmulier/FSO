f13efaf6a4b897ea490d3927f43ff83d01523dc1
kevinmulier <kevin.mulier@outlook.fr>
2024-01-15 16:44:33 +0100

    complete exercise 3.13

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
 const morgan = require("morgan");
 const app = express();
+
+const Person = require("./models/person");
 
 app.use(cors());
 app.use(express.static("dist"));
@@ -18,31 +21,10 @@ app.use(
 	),
 );
 
-let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: 4,
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-];
-
 app.get("/api/persons", (request, response) => {
-	response.json(persons);
+	Person.find({}).then((persons) => {
+		response.json(persons);
+	});
 });
 
 app.get("/api/persons/:id", (request, response) => {
