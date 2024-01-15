22736e6f00a621bf47ea2354a0c7c3d31c6490b9
kevinmulier <kevin.mulier@outlook.fr>
2024-01-15 21:55:25 +0100

    complete exercise 3.16

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -21,19 +21,23 @@ app.use(
 	),
 );
 
-app.get("/api/persons", (request, response) => {
-	Person.find({}).then((persons) => {
-		response.json(persons);
-	});
+app.get("/api/persons", (request, response, next) => {
+	Person.find({})
+		.then((persons) => {
+			response.json(persons);
+		})
+		.catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-	Person.findById(request.params.id).then((person) => {
-		response.json(person);
-	});
+app.get("/api/persons/:id", (request, response, next) => {
+	Person.findById(request.params.id)
+		.then((person) => {
+			response.json(person);
+		})
+		.catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
 	const body = request.body;
 
 	if (!body.name) {
@@ -53,26 +57,51 @@ app.post("/api/persons", (request, response) => {
 		number: body.number,
 	});
 
-	newPerson.save().then((savedPerson) => {
-		response.json(savedPerson);
-	});
+	newPerson
+		.save()
+		.then((savedPerson) => {
+			response.json(savedPerson);
+		})
+		.catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-	Person.findByIdAndDelete(request.params.id).then((result) => {
-		response.status(204).end();
-	});
+app.delete("/api/persons/:id", (request, response, next) => {
+	Person.findByIdAndDelete(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
 });
 
-app.get("/info", (request, response) => {
+app.get("/info", (request, response, next) => {
 	const date = new Date();
-	Person.find({}).then((persons) => {
-		response.send(`
+	Person.find({})
+		.then((persons) => {
+			response.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${date}</p>
     `);
-	});
+		})
+		.catch((error) => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === "CastError") {
+		return response.status(400).send({ error: "malformatted id" });
+	}
+
+	next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = 3001;
 app.listen(PORT, () => {
