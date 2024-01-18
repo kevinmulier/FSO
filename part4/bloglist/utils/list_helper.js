5463172ffc4de8e8f58acd553a5e9d72f74e33f0
kevinmulier <kevin.mulier@outlook.fr>
2024-01-18 17:19:24 +0100

    add tests for returning most liked blog function

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,13 +2,26 @@ const dummy = (blogs) => {
   return 1;
 };
 
-const totalLikes = (posts) => {
-  const sum = posts.reduce((acc, c) => acc + c.likes, 0);
+const totalLikes = (blogs) => {
+  const sum = blogs.reduce((acc, c) => acc + c.likes, 0);
 
   return sum;
+};
+
+const blogWithMostLikes = (blogs) => {
+  const mostLikedBlog = blogs.sort((a, b) => b.likes - a.likes)[0];
+
+  return mostLikedBlog
+    ? {
+        title: mostLikedBlog.title,
+        author: mostLikedBlog.author,
+        likes: mostLikedBlog.likes,
+      }
+    : {};
 };
 
 module.exports = {
   dummy,
   totalLikes,
+  blogWithMostLikes,
 };
