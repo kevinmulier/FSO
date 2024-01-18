290c79feafb856e88d76d99b146dd4068cfdccba
kevinmulier <kevin.mulier@outlook.fr>
2024-01-18 17:03:48 +0100

    add tests for blog posts total likes

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (posts) => {
+  const sum = posts.reduce((acc, c) => acc + c.likes, 0);
+
+  return sum;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
