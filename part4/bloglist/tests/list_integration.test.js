b970df9a6b91065f73b6327bd368f981834292bf
kevinmulier <kevin.mulier@outlook.fr>
2024-01-23 18:05:23 +0100

    add delete route and related tests

--- part4/bloglist/tests/list_integration.test.js
+++ part4/bloglist/tests/list_integration.test.js
@@ -20,86 +20,105 @@ beforeEach(async () => {
   }
 });
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+describe('when there is initially some notes saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
+
+    expect(response.body).toHaveLength(blogs.length);
+  });
+
+  test('unique identifier for blog is id property', async () => {
+    const response = await api.get('/api/blogs');
+
+    expect(response.body[0].id).toBeDefined();
+  });
 });
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs');
+describe('when adding a new blog', () => {
+  test('new blog get saved', async () => {
+    const newBlog = {
+      title: 'new blog',
+      author: 'Stringer',
+      url: 'Stringy',
+      likes: 0,
+    };
 
-  expect(response.body).toHaveLength(blogs.length);
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await api.get('/api/blogs');
+    expect(blogsAtEnd.body).toHaveLength(blogs.length + 1);
+
+    const titles = blogsAtEnd.body.map((r) => r.title);
+    expect(titles).toContain('new blog');
+  });
+
+  test('new blog without likes property still get 0 in likes property', async () => {
+    const newBlog = {
+      title: 'new blog',
+      author: 'Stringer',
+      url: 'Stringy',
+    };
+
+    const savedBlog = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    expect(savedBlog.body.likes).toBe(0);
+  });
+
+  test("new blog without title property doesn't get saved", async () => {
+    const newBlog = {
+      author: 'Stringer',
+      url: 'Stringy',
+      likes: 0,
+    };
+
+    await api.post('/api/blogs').send(newBlog).expect(400);
+
+    const blogsAtEnd = await api.get('/api/blogs');
+    expect(blogsAtEnd.body).toHaveLength(blogs.length);
+  });
+
+  test("new blog without url property doesn't get saved", async () => {
+    const newBlog = {
+      title: 'new blog',
+      author: 'Stringer',
+      likes: 0,
+    };
+
+    await api.post('/api/blogs').send(newBlog).expect(400);
+
+    const blogsAtEnd = await api.get('/api/blogs');
+    expect(blogsAtEnd.body).toHaveLength(blogs.length);
+  });
 });
 
-test('unique identifier for blog is id property', async () => {
-  const response = await api.get('/api/blogs');
+describe('when deleting a blog', () => {
+  test('it succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await api.get('/api/blogs');
+    const blogToDelete = blogsAtStart.body[0];
 
-  expect(response.body[0].id).toBeDefined();
-});
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-test('new blog get saved', async () => {
-  const newBlog = {
-    title: 'new blog',
-    author: 'Stringer',
-    url: 'Stringy',
-    likes: 0,
-  };
+    const blogsAtEnd = await api.get('/api/blogs');
+    expect(blogsAtEnd.body).toHaveLength(blogsAtStart.body.length - 1);
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-
-  const blogsAtEnd = await api.get('/api/blogs');
-  expect(blogsAtEnd.body).toHaveLength(blogs.length + 1);
-
-  const titles = blogsAtEnd.body.map((r) => r.title);
-  expect(titles).toContain('new blog');
-});
-
-test('new blog without likes property still get 0 in likes property', async () => {
-  const newBlog = {
-    title: 'new blog',
-    author: 'Stringer',
-    url: 'Stringy',
-  };
-
-  const savedBlog = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-
-  expect(savedBlog.body.likes).toBe(0);
-});
-
-test("new blog without title property doesn't get saved", async () => {
-  const newBlog = {
-    author: 'Stringer',
-    url: 'Stringy',
-    likes: 0,
-  };
-
-  await api.post('/api/blogs').send(newBlog).expect(400);
-
-  const blogsAtEnd = await api.get('/api/blogs');
-  expect(blogsAtEnd.body).toHaveLength(blogs.length);
-});
-
-test("new blog without url property doesn't get saved", async () => {
-  const newBlog = {
-    title: 'new blog',
-    author: 'Stringer',
-    likes: 0,
-  };
-
-  await api.post('/api/blogs').send(newBlog).expect(400);
-
-  const blogsAtEnd = await api.get('/api/blogs');
-  expect(blogsAtEnd.body).toHaveLength(blogs.length);
+    const titles = blogsAtEnd.body.map((r) => r.title);
+    expect(titles).not.toContain(blogToDelete.title);
+  });
 });
 
 afterAll(async () => {
