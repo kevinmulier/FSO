49f22522f45ea7306e3f321c31bbdc9691e922e4
kevinmulier <kevin.mulier@outlook.fr>
2024-01-26 16:53:36 +0100

    add validation for password and uniqueness of username + min length

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
+    minLength: 3,
   },
   name: String,
-  passwordHash: String,
+  passwordHash: {
+    type: String,
+    required: true,
+  },
 });
+
+userSchema.plugin(uniqueValidator);
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
