5b7b3f5e8bca00e8c47d07398f5ebc845b26e28a
kevinmulier <kevin.mulier@outlook.fr>
2024-02-05 18:07:02 +0100

    add e2e test to ensure that user get login form by default when not logged in

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+describe('Blog app', () => {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    cy.visit('http://localhost:5173');
+  });
+
+  it('Login form is shown', () => {
+    cy.contains('Log in to application');
   });
 });
