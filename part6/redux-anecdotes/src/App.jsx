330e493f03ca3b261bc2116a11458fa125a01414
kevinmulier <kevin.mulier@outlook.fr>
2024-02-14 15:14:15 +0100

    separate anecdotes initialization logic from app component

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,19 +1,16 @@
 import { useDispatch } from 'react-redux';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
-import anecdoteService from './services/anecdotes';
 
 import Filter from './components/Filter';
 import Notification from './components/Notification';
 import { useEffect } from 'react';
-import { setAnecdotes } from './reducers/anecdoteReducer';
+import { initializeAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    anecdoteService.getAll().then((anecdotes) => {
-      dispatch(setAnecdotes(anecdotes));
-    });
+    dispatch(initializeAnecdotes());
   }, []);
 
   return (
